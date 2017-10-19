6d60c1fc1f11f8d4f4943d0ee01eabf1bf472dd6
Fernando Cladera <fcladera@fcladera.com>
2017-10-19 11:50:12 -0300

    [Fw] Changed for TOROID board

--- TOROID_board/board.mk
+++ TOROID_board/board.mk
@@ -1,5 +1,5 @@
 # List of all the board related files.
-BOARDSRC = board_BluePill/board.c
+BOARDSRC = TOROID_board/board.c
 
 # Required include directories
-BOARDINC = board_BluePill
+BOARDINC = TOROID_board
